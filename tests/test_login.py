c00b3fb891002ee9167c801259afef556219e733
Serhii <herzikornserhii@gmail.com>
2026-08-08 13:46:55 +0200

    Add login test for saucedemo

---
+++ tests/test_login.py
@@ -0,0 +1,6 @@
+def test_login(page):
+    page.goto("https://www.saucedemo.com")
+    page.fill("#user-name", "standard_user")
+    page.fill("#password", "secret_sauce")
+    page.click("#login-button")
+    assert page.url == "https://www.saucedemo.com/inventory.html"
