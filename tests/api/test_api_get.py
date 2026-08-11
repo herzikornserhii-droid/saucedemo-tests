955368b5e92a576168a9bdf5a9bd780b451762b9
Serhii <herzikornserhii@gmail.com>
2026-08-11 19:36:44 +0200

    Move API key to environment variable

--- tests/api/test_api_get.py
+++ tests/api/test_api_get.py
@@ -1,6 +1,11 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def test_get_user_status_code():
-    headers = {"x-api-key": "free_user_3HmLPRzMqYlxe26R3wTx0QGmwCx"}
+    api_key = os.getenv("REQRES_API_KEY")
+    headers = {"x-api-key": api_key}
     response = requests.get("https://reqres.in/api/users/2", headers=headers)
     assert response.status_code == 200
