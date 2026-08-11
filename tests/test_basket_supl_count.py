454f7c0d063ab8a36549972d0b134b51b8f5d3d9
Serhii <herzikornserhii@gmail.com>
2026-08-11 09:40:49 +0200

    Add logged_in_page fixture in conftest, refactor cart test

--- tests/test_basket_supl_count.py
+++ tests/test_basket_supl_count.py
@@ -1,13 +1,7 @@
-from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 
 
-def test_add_to_cart(page):
-    login_page = LoginPage(page)
-    login_page.open()
-    login_page.login("standard_user", "secret_sauce")
-
-    inventory_page = InventoryPage(page)
+def test_add_to_cart(logged_in_page):
+    inventory_page = InventoryPage(logged_in_page)
     inventory_page.add_backpack_to_cart()
-
     assert inventory_page.get_cart_count() == "1"
